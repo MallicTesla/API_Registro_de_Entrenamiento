03a24d1a3b55fd795523a28ed1f084b4d746fbab
MallicTesla <el_mallic@hotmail.com>
2024-10-30 17:26:25 -0300

    iniciando el proyecto

---
+++ api_registro_entrenamiento/src/main/java/com/mallic_corp/api_registro_entrenamiento/ApiRegistroEntrenamientoApplication.java
@@ -0,0 +1,13 @@
+package com.mallic_corp.api_registro_entrenamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistroEntrenamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistroEntrenamientoApplication.class, args);
+	}
+
+}
---
+++ api_registro_entrenamiento/src/test/java/com/mallic_corp/api_registro_entrenamiento/ApiRegistroEntrenamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallic_corp.api_registro_entrenamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistroEntrenamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
